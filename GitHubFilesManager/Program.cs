87edd4f1988bd5bb431eb451e475990880fa2bc5
danperunov <danperunov@gmail.com>
2022-06-28 18:16:49 +0300

    Finishing first steps

--- GitHubFilesManager/Program.cs
+++ GitHubFilesManager/Program.cs
@@ -1,6 +1,32 @@
-var builder = WebApplication.CreateBuilder(args);
+using System.Text;
+using Octokit;
+
+var gitHubClient = new GitHubClient(new ProductHeaderValue("MyCoolFilesManager"));
+gitHubClient.Credentials = new Credentials("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");
+
+var user = await gitHubClient.User.Get("danperunov");
+
+var sb = new StringBuilder("---");
+sb.AppendLine();
+sb.AppendLine($"date: \"2021-05-01\"");
+sb.AppendLine($"title: \"My new fancy post\"");
+sb.AppendLine("tags: [csharp, azure, dotnet]");
+sb.AppendLine("---");
+sb.AppendLine();
+
+sb.AppendLine("# The heading for my first post");
+sb.AppendLine();
+
+
+
+
+
+
+
+/*var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
 
 app.Run();
+*/
