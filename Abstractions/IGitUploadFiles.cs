2e308e492f7c5fa9fefd98d0b0e1b03c1db17bfc
danperunov <danperunov@gmail.com>
2022-07-02 18:49:31 +0300

    removing space

--- Abstractions/IGitUploadFiles.cs
+++ Abstractions/IGitUploadFiles.cs
@@ -2,6 +2,5 @@
 
 public interface IGitUploadFiles
 {
-    public Task UploadFile(string login, string repo, string branch, string? targetFile, string userToken,
-        string filePath);
+    public Task UploadFile(string login, string repo, string branch, string? targetFile, string userToken, string filePath);
 }
